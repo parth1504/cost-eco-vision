6cf70ec57c861d9295dacc46fdf721ec7c60b7ba
parth1504 <parh1504@gmail.com>
2026-05-16 22:56:09 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { CostOptimization } from "@/pages/CostOptimization";
 import { Security } from "@/pages/Security";
 import { Settings } from "@/pages/Settings";
 import { LogAnalyser } from "@/pages/LogAnalyser";
-import { LogAnalyzer } from "@/pages/LogAnalyzer";
+// import { LogAnalyzer } from "@/pages/LogAnalyzer";
 import { PRReviewer } from "@/pages/PRReviewer";
 import NotFound from "./pages/NotFound";
 
@@ -32,7 +32,7 @@ const App = () => (
             <Route path="/security" element={<Security />} />
             <Route path="/settings" element={<Settings />} />
             <Route path="/log-analyser" element={<LogAnalyser />} />
-            <Route path="/engineering/log-analyzer" element={<LogAnalyzer />} />
+            <Route path="/engineering/log-analyzer" element={<LogAnalyser />} />
             <Route path="/engineering/pr-reviewer" element={<PRReviewer />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
